83d0bf7c2b3d619758f818452c8a4f9f5d89f496
nainnasyuhada <47973250+nainnasyuhada@users.noreply.github.com>
2019-02-27 15:30:04 +0800

    Update App.js

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Hello World!</Text>
+        <Text>Nurul Ainna Syuhada Binti Norazmi 1520592</Text>
       </View>
     );
   }
